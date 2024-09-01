ca3d27f48a960a250ccd518d60dcb68a6ee3eb1c
Fadelveria <hasanfadhli699@gmail.com>
2024-09-01 16:44:46 +0700

    upload tugas 2

---
+++ Praktikan/DataDiri/src/main/java/com/mycompany/datadiri/DataDiri.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.datadiri;
+
+/**
+ *
+ * @author Organizer
+ */
+
+
+
+public class DataDiri {
+    public static void main(String[] args) {
+        // Menampilkan informasi dasar
+        System.out.println("Nama : Hasan Fadhli Robbi");
+        System.out.println("NPM : 2340506080");
+        System.out.println("Alamat : Nusantara");
+        System.out.println("No HP : 1");
+    }
+}
+
